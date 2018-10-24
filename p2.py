1cbae987da3abf5f68593ce05a73e323a7732e48
Grey Wang <blacksilvercoin@outlook.com>
2018-10-24 14:17:15 +0800

    for the rest

--- p2.py
+++
@@ -1,9 +0,0 @@
-#练习2
-
-bacon="a cool man"
-answer= input("please input the answer:")
-while True:
-    if answer==bacon:
-        break
-    answer= input("sorry,the answer is not correct:")
-print("your answer is correct")
--- price.py
+++
@@ -1,9 +0,0 @@
-weight = int(input())
-if weight <= 10:
-	print("3.50")
-else:
-	n = weight - 10
-	p1 = 0.75 * n
-	pt = p1 + 3.50
-	print(pt)
-
